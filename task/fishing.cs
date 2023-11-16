4971bdabc9aeee613f2be597ca9016fc2845b245
kamilyashakirova <144135244+kamilyashakirova@users.noreply.github.com>
2023-11-16 12:42:46 +0300

    Update fishing.cs

--- task/fishing.cs
+++ task/fishing.cs
@@ -3,6 +3,6 @@ public class Fishing : Game
 {
     public override void Play(string tname)
     {
-        Console.WriteLine("Команда {0} играет в рыбалку", tname);
+        Console.WriteLine("команда {0} играет в рыбалку", tname);
     }
 }
