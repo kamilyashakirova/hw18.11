2c0ed48540366187b05e22760b6a878e7f48241b
kamilyashakirova <144135244+kamilyashakirova@users.noreply.github.com>
2023-11-16 12:44:01 +0300

    Update beach.cs

--- task/beach.cs
+++ task/beach.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 public class BeachGame : Game
 {
-    public override void Play(string teamName)
+    public override void Play(string tname)
     {
-        Console.WriteLine("Команда {0} играет на пляже", teamName);
+        Console.WriteLine("команда {0} играет на пляже", tname);
     }
 }
