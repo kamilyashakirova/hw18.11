03894424deb614af92495d5edc94098e310067f4
kamilyashakirova <144135244+kamilyashakirova@users.noreply.github.com>
2023-11-16 12:41:15 +0300

    Update postman.cs

--- task/postman.cs
+++ task/postman.cs
@@ -3,6 +3,6 @@ public class Postman : Game
 {
     public override void Play(string tname)
     {
-        Console.WriteLine("Команда {0} играет в почтальона", tname);
+        Console.WriteLine("команда {0} играет в почтальона", tname);
     }
 }
